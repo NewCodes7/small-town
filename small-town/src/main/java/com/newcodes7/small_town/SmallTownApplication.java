cc338678f3ca43bd71db4e150ba4e40f9a76e410
Sanghyeok Kim <newcodes7@gmail.com>
2025-05-17 20:23:26 +0900

    init: spring boot 프로젝트 초기 설정

---
+++ small-town/src/main/java/com/newcodes7/small_town/SmallTownApplication.java
@@ -0,0 +1,13 @@
+package com.newcodes7.small_town;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallTownApplication.class, args);
+	}
+
+}
