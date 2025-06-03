37d2098eca9257423750eb81aa6cfc26f860bdc9
Sanghyeok Kim <newcodes7@gmail.com>
2025-06-03 11:34:49 +0000

    init: 초기 개발 환경 세팅 (MySQL 포함)

---
+++ src/main/java/com/newcodes7/small_town/HelloController.java
@@ -0,0 +1,13 @@
+package com.newcodes7.small_town.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/com/newcodes7/small_town/SmallTownApplicationTests.java
@@ -0,0 +1,12 @@
+package com.newcodes7.small_town;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallTownApplicationTests {
+	@Test
+	void contextLoads() {
+		// This test will simply check if the application context loads successfully.
+	}
+}
