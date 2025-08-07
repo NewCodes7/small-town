c72f61304c349649026a551a9e44a7ed5c0ace9e
Sanghyeok Kim <newcodes7@gmail.com>
2025-08-07 10:12:45 +0000

    feat: http로 접속 시 https로 리다이렉트

---
+++ src/main/java/com/newcodes7/small_town/config/HttpToHttpsRedirectConfig.java
@@ -0,0 +1,27 @@
+package com.newcodes7.small_town.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpToHttpsRedirectConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(80); 
+        connector.setSecure(false);
+        connector.setRedirectPort(443); 
+        return connector;
+    }
+}
