cf6f2b2ff719f76cf1ea2d3f2d95078a55268bc5
Sanghyeok Kim <newcodes7@gmail.com>
2025-08-15 06:51:09 +0000

    fix: 캐시 만료기간 1시간으로 수정

--- src/main/java/com/newcodes7/small_town/global/cache/HomeCacheInterceptor.java
+++ src/main/java/com/newcodes7/small_town/global/cache/HomeCacheInterceptor.java
@@ -8,9 +8,6 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.time.Duration;
-import java.time.ZonedDateTime;
-import java.time.ZoneId;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -18,14 +15,7 @@ public class HomeCacheInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
-        ZonedDateTime nextRenewal = now.plusDays(0)
-                                        .withHour(1)
-                                        .withMinute(0)
-                                        .withSecond(0)
-                                        .withNano(0);
-        Duration duration = Duration.between(now, nextRenewal);
-        long maxAgeInSeconds = duration.getSeconds();
+        long maxAgeInSeconds = TimeUnit.HOURS.toSeconds(1);
         
         response.setHeader("Cache-Control", "public, max-age=" + maxAgeInSeconds);
         response.setDateHeader("Expires", TimeUnit.SECONDS.toMillis(maxAgeInSeconds) + System.currentTimeMillis());
