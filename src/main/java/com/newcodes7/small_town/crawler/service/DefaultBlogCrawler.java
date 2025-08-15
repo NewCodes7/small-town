96652d8f8a036aca40642b5fd91c97537a899952
Sanghyeok Kim <newcodes7@gmail.com>
2025-08-15 10:31:52 +0000

    fix: 뱅크샐러드 발행일 파싱 태그 수정

--- src/main/java/com/newcodes7/small_town/crawler/service/DefaultBlogCrawler.java
+++ src/main/java/com/newcodes7/small_town/crawler/service/DefaultBlogCrawler.java
@@ -219,9 +219,9 @@ public class DefaultBlogCrawler implements BlogCrawler {
                                 .toFormatter(Locale.ENGLISH);
                 LocalDate date = LocalDate.parse(dateText, formatter);
                 publishedAt = date.atStartOfDay();
-            } else if (parsingSelector.getPublishFormat().equals("d MMMM")) {
-                String cleanDateText = publishElement.text(); // "3 August"
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM", Locale.ENGLISH);
+            } else if (parsingSelector.getPublishFormat().equals("dd MMM")) {
+                String cleanDateText = publishElement.text(); // "03 Aug"
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM", Locale.ENGLISH);
                 TemporalAccessor temporalAccessor = formatter.parse(cleanDateText);
                 LocalDate date = LocalDate.of(LocalDate.now().getYear(),
                                             temporalAccessor.get(ChronoField.MONTH_OF_YEAR),
