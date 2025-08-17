7e3d15b994946baf6b9f15ec631d3ba2d0763850
Sanghyeok Kim <newcodes7@gmail.com>
2025-08-17 00:06:40 +0000

    test: ArticleService 게시글 리스트 조회 테스트 케이스 추가

--- src/main/java/com/newcodes7/small_town/article/dto/GroupedArticlesDto.java
+++ src/main/java/com/newcodes7/small_town/article/dto/GroupedArticlesDto.java
@@ -2,9 +2,11 @@ package com.newcodes7.small_town.article.dto;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
--- src/test/java/com/newcodes7/small_town/article/service/ArticleServiceTest.java
+++ src/test/java/com/newcodes7/small_town/article/service/ArticleServiceTest.java
@@ -2,7 +2,6 @@ package com.newcodes7.small_town.article.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -16,14 +15,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.TestPropertySource;
 
 import com.newcodes7.small_town.article.dto.ArticleListResponseDto;
 import com.newcodes7.small_town.article.dto.ArticleResponseDto;
+import com.newcodes7.small_town.article.dto.CorporationDto;
 import com.newcodes7.small_town.article.dto.GroupedArticlesDto;
 import com.newcodes7.small_town.article.entity.Article;
 import com.newcodes7.small_town.article.entity.Corporation;
@@ -42,12 +42,7 @@ public class ArticleServiceTest {
     public void 게시글_리스트_조회() {
         //given
         String view = "list";
-        List<Article> articlesList = createArticles(
-            Map.of(
-                1L, 1L,
-                2L, 1L
-            )
-        );
+        List<Article> articlesList = createArticles(Map.of(1L, 1L, 2L, 1L));
         Pageable pageable = PageRequest.of(0, 10);
         Page<Article> articles = new PageImpl<>(articlesList, pageable, articlesList.size());
         when(articleRepository.findArticlesWithFilters(null, null, null, pageable)).thenReturn(articles);
@@ -60,20 +55,117 @@ public class ArticleServiceTest {
     }
 
     @Test
+    public void 게시글_리스트_조회_키워드() {
+        //given
+        String view = "list";
+        String keyword = "1편";
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Article> targetArticles = createArticles(Map.of(1L, 1L));
+        Page<Article> expect = new PageImpl<>(targetArticles, pageable, targetArticles.size());
+        when(articleRepository.findArticlesWithFilters(keyword, null, null, pageable)).thenReturn(expect);
+
+        //when
+        Page<ArticleResponseDto> result = articleService.getArticlesWithFilters(keyword, null, 0, 10, null, view);
+
+        //then
+        assertPageEquals(expect, result);
+    }
+
+    @Test
+    public void 게시글_리스트_조회_국내() {
+        //given
+        String view = "list";
+        String keyword = "1편";
+        List<String> regions = Arrays.asList("domestic");
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Article> articlesList = createArticles(Map.of(1L, 1L, 2L, 1L));
+        Page<Article> expect = new PageImpl<>(articlesList, pageable, articlesList.size());
+        when(articleRepository.findArticlesWithFilters(keyword, Arrays.asList(true), null, pageable)).thenReturn(expect);
+
+        //when
+        Page<ArticleResponseDto> result = articleService.getArticlesWithFilters(keyword, regions, 0, 10, null, view);
+
+        //then
+        assertPageEquals(expect, result);
+    }
+
+    @Test
+    public void 게시글_리스트_조회_해외() {
+        //given
+        String view = "list";
+        String keyword = "1편";
+        List<String> regions = Arrays.asList("overseas");
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Article> articlesList = createArticles(Map.of(1L, 1L, 2L, 1L));
+        Page<Article> expect = new PageImpl<>(articlesList, pageable, articlesList.size());
+        when(articleRepository.findArticlesWithFilters(keyword, Arrays.asList(false), null, pageable)).thenReturn(expect);
+
+        //when
+        Page<ArticleResponseDto> result = articleService.getArticlesWithFilters(keyword, regions, 0, 10, null, view);
+
+        //then
+        assertPageEquals(expect, result);
+    }
+
+    @Test
+    public void 게시글_리스트_조회_국내_해외() {
+        //given
+        String view = "list";
+        String keyword = "1편";
+        List<String> regions = Arrays.asList("domestic", "overseas");
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Article> articlesList = createArticles(Map.of(1L, 1L, 2L, 1L));
+        Page<Article> expect = new PageImpl<>(articlesList, pageable, articlesList.size());
+        when(articleRepository.findArticlesWithFilters(keyword, Arrays.asList(true, false), null, pageable)).thenReturn(expect);
+
+        //when
+        Page<ArticleResponseDto> result = articleService.getArticlesWithFilters(keyword, regions, 0, 10, null, view);
+
+        //then
+        assertPageEquals(expect, result);
+    }
+
+    @Test
+    public void 게시글_리스트_조회_국내_검색() {
+        //given
+        String view = "list";
+        String keyword = "1편";
+        List<String> regions = Arrays.asList("domestic");
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Article> targetArticles = createArticles(Map.of(1L, 1L));
+        Page<Article> expect = new PageImpl<>(targetArticles, pageable, targetArticles.size());
+        when(articleRepository.findArticlesWithFilters(keyword, Arrays.asList(true), null, pageable)).thenReturn(expect);
+
+        //when
+        Page<ArticleResponseDto> result = articleService.getArticlesWithFilters(keyword, regions, 0, 10, null, view);
+
+        //then
+        assertPageEquals(expect, result);
+    }
+
+    @Test
     public void 게시글_기업별_조회() {
         //given
         String view = "grouped";
-        List<Long> corporationsIdList = Arrays.asList(1L, 1L, 2L, 2L);
-        List<Article> allArticles = createArticles(
-            Map.of(
-                1L, 1L,
-                2L, 1L,
-                3L, 2L,
-                4L, 2L
-            )
-        );
+        List<Long> corporationsIdList = Arrays.asList(1L, 2L);
         Pageable pageable = PageRequest.of(0, 10);
         Page<Long> corporationIds = new PageImpl<>(corporationsIdList, pageable, corporationsIdList.size());
+        List<Article> articlesCorp1 = createArticles(Map.of(1L, 1L, 2L, 1L));
+        List<Article> articlesCorp2 = createArticles(Map.of(3L, 2L, 4L, 2L));
+        List<Article> allArticles = new ArrayList<>();
+        allArticles.addAll(articlesCorp1);
+        allArticles.addAll(articlesCorp2);
+        GroupedArticlesDto groupedArticlesDto1 = GroupedArticlesDto.builder()
+            .corporation(new CorporationDto(createCorporation(1L)))
+            .articles(articlesCorp1.stream().map(ArticleListResponseDto::new).collect(Collectors.toList()))
+            .build();
+        GroupedArticlesDto groupedArticlesDto2 = GroupedArticlesDto.builder()
+            .corporation(new CorporationDto(createCorporation(2L)))
+            .articles(articlesCorp2.stream().map(ArticleListResponseDto::new).collect(Collectors.toList()))
+            .build();
+        List<GroupedArticlesDto> groupedList = Arrays.asList(groupedArticlesDto1, groupedArticlesDto2);
+        Page<GroupedArticlesDto> expected = new PageImpl<>(groupedList, pageable, groupedList.size());
+
         when(articleRepository.findCorporationIdsWithFilters(null, null, pageable)).thenReturn(corporationIds);
         when(articleRepository.findArticlesByCorporations(corporationIds.getContent(), null, null)).thenReturn(allArticles);
 
@@ -81,12 +173,28 @@ public class ArticleServiceTest {
         Page<ArticleResponseDto> result = articleService.getArticlesWithFilters(null, null, 0, 10, null, view);
 
         //then
-        GroupedArticlesDto groupedArticlesDto1 = (GroupedArticlesDto) result.getContent().get(0); 
-        GroupedArticlesDto groupedArticlesDto2 = (GroupedArticlesDto) result.getContent().get(1);
-        assertEquals(groupedArticlesDto1.getArticles().get(0).getId(), allArticles.get(3).getId());
-        assertEquals(groupedArticlesDto1.getArticles().get(1).getId(), allArticles.get(2).getId());
-        assertEquals(groupedArticlesDto2.getArticles().get(0).getId(), allArticles.get(1).getId());
-        assertEquals(groupedArticlesDto2.getArticles().get(1).getId(), allArticles.get(0).getId());
+        assertGroupedEquals(expected, result);
+    }
+
+    private void assertGroupedEquals(Page<GroupedArticlesDto> expectedPage, Page<ArticleResponseDto> actualPage) {
+        assertEquals(expectedPage.getTotalElements(), actualPage.getTotalElements());
+        assertEquals(expectedPage.getNumber(), actualPage.getNumber());
+        assertEquals(expectedPage.getSize(), actualPage.getSize());
+        assertEquals(expectedPage.getTotalPages(), actualPage.getTotalPages());
+        assertEquals(expectedPage.getContent().size(), actualPage.getContent().size());
+
+        for (int i = 0; i < expectedPage.getContent().size(); i++) {
+            assertGroupedDtoEquals(expectedPage.getContent().get(i), (GroupedArticlesDto) actualPage.getContent().get(i));
+        }
+    }
+
+    private void assertGroupedDtoEquals(GroupedArticlesDto expect, GroupedArticlesDto actual) {
+        assertEquals(expect.getCorporation().getId(), actual.getCorporation().getId());
+
+        for (int i = 0; i < expect.getArticles().size(); i++) {
+            assertEquals(expect.getArticles().get(i).getId(), actual.getArticles().get(i).getId());
+            assertEquals(expect.getArticles().get(i).getTitle(), actual.getArticles().get(i).getTitle());
+        }
     }
 
     private List<Article> createArticles(Map<Long, Long> articles) {
@@ -106,14 +214,14 @@ public class ArticleServiceTest {
     private Article createArticle(long articleId, Corporation corporation) {
         return Article.builder()
             .id(articleId)
-            .title("백엔드 업무일지" + articleId)
+            .title("백엔드 업무일지" + articleId + "편")
             .summary("스프링 부트 트러블슈팅을 다뤄봐요~" + articleId)
             .link("https://newcodes.net/blogs/" + articleId)
             .viewCount((int) Math.random() * 100)
             .likeCount((int) Math.random() * 100)
             .thumbnailImage("https://newcodes.net/thumbnail/" + articleId)
             .readingTime((int) Math.random() * 100)
-            .publishedAt(LocalDateTime.now().plusHours(articleId))
+            .publishedAt(LocalDateTime.now().minusHours(articleId))
             .corporation(corporation)
             .articleTags(new ArrayList<>())
             .build();
@@ -124,9 +232,8 @@ public class ArticleServiceTest {
         assertEquals(expectedPage.getNumber(), actualPage.getNumber());
         assertEquals(expectedPage.getSize(), actualPage.getSize());
         assertEquals(expectedPage.getTotalPages(), actualPage.getTotalPages());
-        
         assertEquals(expectedPage.getContent().size(), actualPage.getContent().size());
-        
+
         for (int i = 0; i < expectedPage.getContent().size(); i++) {
             assertArticleEqualsDto(expectedPage.getContent().get(i), (ArticleListResponseDto) actualPage.getContent().get(i));
         }
