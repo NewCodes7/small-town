7ceca09d71f42ab28c8edb6d20ef5b1c3c94e209
Sanghyeok Kim <newcodes7@gmail.com>
2025-08-18 23:57:41 +0000

    fix: 스케줄링 설정 허용함으로써 안 되는 문제 해결

--- src/main/java/com/newcodes7/small_town/SmallTownApplication.java
+++ src/main/java/com/newcodes7/small_town/SmallTownApplication.java
@@ -3,9 +3,11 @@ package com.newcodes7.small_town;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableConfigurationProperties
 @SpringBootApplication
+@EnableScheduling
 public class SmallTownApplication {
 
 	public static void main(String[] args) {
