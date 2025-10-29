9e26efc631f3d530b0580263b2eef687fc3e1a16
Sanghyeok Kim <newcodes7@gmail.com>
2025-10-29 09:38:29 +0000

    refactor: html 캐시 1시간에서 5분으로 변경

--- src/main/java/com/newcodes7/small_town/global/cache/HomeCacheInterceptor.java
+++ src/main/java/com/newcodes7/small_town/global/cache/HomeCacheInterceptor.java
@@ -15,7 +15,7 @@ public class HomeCacheInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        long maxAgeInSeconds = TimeUnit.HOURS.toSeconds(1);
+        long maxAgeInSeconds = TimeUnit.MINUTES.toSeconds(5);
         
         response.setHeader("Cache-Control", "public, max-age=" + maxAgeInSeconds);
         response.setDateHeader("Expires", TimeUnit.SECONDS.toMillis(maxAgeInSeconds) + System.currentTimeMillis());
